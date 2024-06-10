65316822e1607d1bf7234d2312248b42a022c3a3
suhzeet <sujeetneupane28@gmail.com>
2024-06-10 21:45:29 +0545

    html and lit css done

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState } from "react";
 
 function ToDoList() {
@@ -23,27 +22,57 @@ function ToDoList() {
 
   return (
     <>
-      <div className=" text-center">
-        <h1 className="text-white text-4xl">To-Do List</h1>
-        <div>
-          <input
-            type="text"
-            placeholder="Enter a Task.."
-            value={newTasks}
-            onChange={handleInputChange}
-          />
-          <button onClick={addTask} className=" bg-orange-400">
-            Add
-          </button>
-        </div>
-        <div>
-          <ol>
-            {tasks.map((task, index) => (
-              <li className="my-4" key={index}>
-                <span className=" text-white">{task}</span>
-              </li>
-            ))}
-          </ol>
+      <div className="grid justify-center mx-auto max-w-2xl">
+        <div className="w-full">
+          <h1 className="text-white text-4xl my-8 border-solid border-b-2 border-gray-400">
+            To-Do List
+          </h1>
+          <div>
+            <input
+              type="text"
+              placeholder="Enter a Task.."
+              className=" rounded-l-md py-1 px-3"
+              value={newTasks}
+              onChange={handleInputChange}
+            />
+            <button
+              onClick={addTask}
+              className=" bg-orange-400 py-1 px-3 rounded-r-md"
+            >
+              Add
+            </button>
+          </div>
+          <div>
+            <ol>
+              {tasks.map((task, index) => (
+                <li className="my-4 flex justify-between" key={index}>
+                  <span className=" text-white">
+                    {index + 1}. {task}
+                  </span>
+                  <div className="flex gap-2">
+                    <button
+                      className=" hover:opacity-70"
+                      onClick={() => deleteTask(index)}
+                    >
+                      ❎
+                    </button>
+                    <button
+                      className=" hover:opacity-70"
+                      onClick={() => moveTaskUp(index)}
+                    >
+                      👆🏻
+                    </button>
+                    <button
+                      className=" hover:opacity-70"
+                      onClick={() => moveTaskDown(index)}
+                    >
+                      👇
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ol>
+          </div>
         </div>
       </div>
     </>
